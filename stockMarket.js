e6ecb33265e3b35ac99cc23938a8fc636c5c0c01
Aman Agrawal <amantheboss1@gmail.com>
2024-06-30 20:55:18 +0530

    added stockMarket.js

---
+++ stockMarket.js
@@ -0,0 +1,11 @@
+import { addStockData} from "./buttonDiv.js";
+// import { getProfile } from "./profile.js";
+
+
+
+const Stocks = ['AAPL','MSFT','GOOGL','AMZN','PYPL','TSLA','JPM','NVDA','NFLX','DIS'];
+
+ addStockData(Stocks);
+
+
+
